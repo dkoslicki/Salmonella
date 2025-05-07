6346ffe7960a85bed45b1647a16af6bbc14817b4
David Koslicki <dmkoslicki@gmail.com>
2025-05-07 17:08:20 -0400

    Update the silly little wrapper to sourmash manysketch

--- README.md
+++ README.md
@@ -18,6 +18,7 @@ and location of directories
 ./download.sh  # downloads the Salmonella genomes
 ./generate_manifest.sh  # Creates a manifest (list of file names etc.) in prep for `sourmash manysketch` 
 ./make_sketches.sh  # Uses `sourmash manysketch` to sketch all the Salmonella genomes
+# The below may change based on the update to the new Mahmudur code
 ./split_to_uncompressed.sh # Takes the single zip file of sketches and turns it into a bunch of individual, uncompressed signatures
 ./extract_target_metadata.sh 
 ./calculate_ani.sh
--- scripts/make_sketches.sh
+++ scripts/make_sketches.sh
@@ -1,3 +1,37 @@
-#!/bin/bash
-dataDir="/scratch/dmk333_new/temp/Salmonella/data"
-sourmash scripts manysketch -o /scratch/dmk333_new/temp/Salmonella/data/salmonella_k31.sig.zip -p dna,k=31,scaled=100 -c 50 -f /scratch/dmk333_new/temp/Salmonella/data/input/manifest.csv
+#!/usr/bin/env bash
+# make_sketches.sh – run sourmash manysketch for a dataset. This is a silly little wrapper for sourmash
+set -euo pipefail
+
+usage() {
+  cat <<EOF >&2
+Usage: $(basename "$0") -d DATA_DIR [-o OUTPUT.zip] [-m MANIFEST.csv]
+                       [-k KSIZE] [-s SCALED] [-c CORES]
+Defaults: KSIZE=31  SCALED=100  CORES=50
+EOF
+  exit 1
+}
+
+# defaults
+k=31 scaled=100 cores=50
+
+while getopts ":d:o:m:k:s:c:h" opt; do
+  case $opt in
+    d) data=$(realpath "${OPTARG%/}") ;;
+    o) out=$OPTARG ;;
+    m) manifest=$OPTARG ;;
+    k) k=$OPTARG ;;
+    s) scaled=$OPTARG ;;
+    c) cores=$OPTARG ;;
+    h|*) usage ;;
+  esac
+done
+[[ ${data:-} ]] || usage
+
+out=${out:-"$data/salmonella_k${k}.sig.zip"}
+manifest=${manifest:-"$data/input/manifest.csv"}
+
+sourmash scripts manysketch \
+  -o "$out" \
+  -p "dna,k=${k},scaled=${scaled}" \
+  -c "$cores" \
+  -f "$manifest"
